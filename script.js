1c3473059216698f09c4fcf536078430fe5cd00c
zhangoose <az177@eden.rutgers.edu>
2014-02-21 19:14:28 -0500

    added more week2 things, prettified the calculator

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 var sum = 0;
 var displayText = "";
 
-/*
-$( "buton" ).each(function( index ) {
-		console.log( index + ": " + $( this ).text() );
+
+$("buton").each(function(index) {
+		console.log( index + ": " + $(this).text() );
 });
-*/
+
